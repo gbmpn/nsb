2b5f083c8070c99091b5ef62d3a317dd3b5b4730
Georges <37208616+gbmpn@users.noreply.github.com>
2022-09-27 05:07:47 +0200

    Initial commit

---
+++ pages/contact.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        <div class="grid">
+            <div class="col-12 col-md-8">I'm a column 2/3 wide</div>
+            <div class="col-12 col-md-4">I'm a column 1/3 wide</div>
+        </div>
+    </div>
+</template>
